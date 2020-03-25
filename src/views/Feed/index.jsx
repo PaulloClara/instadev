b8d512903127460885823eccfd79a6d7d9d7f4b5
paulloclara <paulloclara@gmail.com>
2020-03-25 02:53:14 -0300

    :rocket: Update like and post in real time

--- src/views/Feed/index.jsx
+++ src/views/Feed/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 
 import Api from "../../services/api";
 import Post from "../../components/Post";
@@ -11,10 +12,27 @@ class Feed extends Component {
   };
 
   async componentDidMount() {
+    this.registerToSocket();
     const response = await Api.get("/posts");
 
     this.setState({ feed: response.data });
   }
+
+  registerToSocket = _ => {
+    const socket = socketIOClient(process.env.REACT_APP_API_URL);
+
+    socket.on("post", newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    });
+
+    socket.on("like", likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post =>
+          post._id === likedPost._id ? likedPost : post
+        )
+      });
+    });
+  };
 
   render() {
     return (
